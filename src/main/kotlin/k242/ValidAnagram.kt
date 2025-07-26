0af9e7795c1bd0e11668c842bc0e9e6c70ad03b5
Tohid Heshmati <tohidfla@gmail.com>
2025-07-26 08:16:38 +0200

    valid anagram my solution

--- src/main/kotlin/k242/ValidAnagram.kt
+++ src/main/kotlin/k242/ValidAnagram.kt
@@ -8,9 +8,26 @@ fun main () {
     println(isAnagram(s1, t1)) // true
     println(isAnagram(s2, t2)) // false
 }
-
-
+// my solution
 fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+    val smap = mutableMapOf<Char, Int>()
+
+    s.forEach { letter ->
+        smap[letter] = smap.getOrDefault(letter, 0) + 1
+    }
+
+    t.forEach { letter ->
+        val toBeReduced = smap.getOrDefault(letter, 0)
+        if (toBeReduced == 0) return false
+        smap[letter] = toBeReduced - 1
+    }
+
+    return true
+}
+
+// best solution
+fun isAnagram2(s: String, t: String): Boolean {
     if (s.length != t.length) {
         return false
     }
